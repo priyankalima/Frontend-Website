bc5c9ab177c539268ffb10c29960789d7637b549
priyankalima <priyankalima99@gmail.com>
2025-01-24 15:01:53 +0530

    commit

--- root.js
+++ root.js
@@ -1,5 +1,6 @@
+// header element
 header.innerHTML = `
-  <nav>
+  <nav class="nav-bar">
      <logo>LOGO</logo>
      <list>
         <a href="/">Home</a>
@@ -8,8 +9,18 @@ header.innerHTML = `
   </nav>
 
 `
-
+// footer element
 footer.innerHTML = `
 copyright@2025
 
 `
+
+// append elments
+var title = document.createElement('title');
+title.innerHTML = "UIUX-Frontend";
+var favIco = document.createElement('link');
+favIco.type = "image/x-icon";
+favIco.rel = "icon";
+favIco.href= "/assets/favicon.png";
+
+document.head.append(title,favIco);
