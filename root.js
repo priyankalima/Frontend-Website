c261c25c076bc04dd12df19e23e29b32112c16d3
priyankalima <priyankalima99@gmail.com>
2025-01-24 13:58:22 +0530

    commit

---
+++ root.js
@@ -0,0 +1,15 @@
+header.innerHTML = `
+  <nav>
+     <logo>LOGO</logo>
+     <list>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+     </list>
+  </nav>
+
+`
+
+footer.innerHTML = `
+copyright@2025
+
+`
